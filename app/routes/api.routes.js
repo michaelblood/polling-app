725cf28ff0d969a8f5d35018896630e56ded0a21
michael <michaelblood@yahoo.com>
2017-02-21 12:05:14 -0600

    more debug2 3 2

--- app/routes/api.routes.js
+++ app/routes/api.routes.js
@@ -27,9 +27,10 @@ module.exports = (app, passport) => {
   app.post('/api/polls/new', isLoggedIn, (req, res) => {
     //let { pollName, options, canAdd } = req.body; // options should be an array
     //redirect to new poll id
-    if ('string' == typeof req.body) req.body = JSON.parse(req.body);
-    console.log(req.body);
-    let { pollname, option1, option2 } = req.body;
+    let body = req.body;
+    if ('string' == typeof req.body) body = JSON.parse(req.body);
+    console.log(body);
+    let { pollname, option1, option2 } = body;
     let options = [option1, option2]; 
     createPoll(req.user._id, pollName, canAdd, options, (err, poll) => {
       if (err) {
