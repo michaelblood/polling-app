b2cf7ccc8a8f636fac6959e2fc821d875b8b2a08
michael <michaelblood@yahoo.com>
2017-02-28 12:02:34 -0600

    rework file structure a bit
    also clean up button formatting for PollInfo component

--- client/src/components/polls/index.js
+++ client/src/components/polls/index.js
@@ -1,13 +1,3 @@
-import React from 'react';
-
 import PollsContainer from './pollsContainer';
 
-const PollsContainerWrapper = React.createClass({
-  render() {
-    return (
-      <PollsContainer params={{filter: this.props.params.filter}} />
-    );
-  }
-});
-
-export default PollsContainerWrapper;
+export default PollsContainer;
